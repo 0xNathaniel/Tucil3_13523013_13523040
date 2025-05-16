4d7467d5e4b8d96ba25debcd03892689d51c5772
Nathaniel Jonathan Rusli <omgitsnathaniels@gmail.com>
2025-05-16 12:36:16 +0700

    refactored: update constructor

--- src/State.java
+++ src/State.java
@@ -1,30 +1,35 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 public class State implements Comparable<State> { //kelas buat nampung state board skrg
     public Board board;
-    public List<Move> moves;
-    public int gValue; //cost so far (moves made)
-    public int hValue; //heuristic value
-    public int fValue; //(g+h for A*)
+    public List<Move> moves; // History dari moves yang sudah dilakukan
+    public int gValue; // Cost so far: untuk UCS dan A*
+    public int hValue; // Heuristic value: untuk A* dan Greedy
+    public int fValue; // Untuk A*: f(n) = g(n) + h(n)
     
-    public State(Board board, List<Move> moves, int hValue) { //buat greedy
+    // Constructor untuk UCS (tidak butuh hValue)
+    public State(Board board, List<Move> moves) {
         this.board = board;
         this.moves = new ArrayList<>(moves);
-        this.gValue = moves.size();
-        this.hValue = hValue;
-        this.fValue = gValue + hValue;
+        calculateGValue();
     }
-    
-    public State(Board board, List<Move> moves, int gValue, int hValue) { // buat A*
+
+    /* Constructor untuk Greedy dan A*  
+     * Perhitungan hValue dijadikan parameter karena terdapat
+     * beberapa metode perhitungn hValue */
+    public State(Board board, List<Move> moves, int hValue) { 
         this.board = board;
         this.moves = new ArrayList<>(moves);
-        this.gValue = gValue;
+        calculateGValue();
         this.hValue = hValue;
-        this.fValue = gValue + hValue;
+        calculateFValue();
     }
     
+    // Getter
     public Board getBoard() {
         return board;
     }
@@ -43,6 +48,81 @@ public class State implements Comparable<State> { //kelas buat nampung state boa
     
     public int getFValue() {
         return fValue;
+    }
+
+    /*Convert state jadi String untuk visitedStates
+    * untuk menghindarkan duplicates dan cycles */
+    public static String getBoardStateString(Board board) {
+        StringBuilder sb = new StringBuilder();
+        for (Car car : board.getCars()) {
+            sb.append(car.getId()).append(car.getX()).append(car.getY());
+        }
+        return sb.toString();
+    }
+
+    // Calculation functions
+    public void calculateGValue() {
+        this.gValue = this.moves.size();
+    }
+
+    // f(n) = g(n) + h(n)
+    public void calculateFValue() {
+        this.fValue = getGValue() + getHValue(); 
+    }
+
+    public static int calculateBlockingCarsHeuristic(Board board) {
+        Car primaryCar = null;
+        for (Car car : board.getCars()) {
+            if (car.isPrimary()) {
+                primaryCar = car;
+                break;
+            }
+        }
+        
+        if (primaryCar == null) return 9999999;
+        
+        int doorX = board.getDoorX();
+        int doorY = board.getDoorY();
+        
+        int distance = 0;
+        
+        if (primaryCar.isHorizontal()) {
+            distance = doorX - (primaryCar.getX() + primaryCar.getLength());
+            
+            Set<Car> blockingCars = new HashSet<>();
+            for (int x = primaryCar.getX() + primaryCar.getLength(); x < doorX; x++) {
+                if (!board.isCellEmpty(x, primaryCar.getY())) {
+                    Car carAtPosition = board.getCarsAt(x, primaryCar.getY());
+                    if (carAtPosition != null) {
+                        blockingCars.add(carAtPosition);
+                    }
+                }
+            }
+            
+            return distance + blockingCars.size() * 2;
+        }  else {
+                distance = doorY - (primaryCar.getY() + primaryCar.getLength());
+                if (doorY == 0) { 
+                    distance = primaryCar.getY();
+                }
+                
+                int blockingCars = 0;
+                if (doorY > primaryCar.getY()) {
+                    for (int y = primaryCar.getY() + primaryCar.getLength(); y < doorY; y++) {
+                        if (!board.isCellEmpty(primaryCar.getX(), y)) {
+                            blockingCars++;
+                        }
+                    }
+                } else {
+                    for (int y = 0; y < primaryCar.getY(); y++) {
+                        if (!board.isCellEmpty(primaryCar.getX(), y)) {
+                            blockingCars++;
+                        }
+                    }
+                }
+            
+            return distance + blockingCars * 2;
+        }
     }
     
     @Override
